5dbb4a9d03d27f32758930e538be9e0dc1f9348a
corinne <yoo77hyeon@gmail.com>
2022-09-17 18:28:54 +0900

    feat: Reflection 학습 테스트 구현 완료

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,30 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
+        // given
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test targetInstance = clazz.getDeclaredConstructor().newInstance();
+        final int expectedExecuteCounts = 2;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // when
+        int actualExecuteCounts = 0;
+        for(Method method : clazz.getDeclaredMethods()) {
+            final MyTest annotation = method.getAnnotation(MyTest.class);
+            if(annotation != null) {
+                method.invoke(targetInstance);
+                actualExecuteCounts++;
+            }
+        }
+
+        // then
+        assertThat(actualExecuteCounts).isEqualTo(expectedExecuteCounts);
     }
 }
--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,19 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.reflections.scanners.Scanners.TypesAnnotated;
 
 class ReflectionsTest {
 
@@ -12,7 +22,23 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
+        Set<Class<?>> controllers = reflections.get(TypesAnnotated.with(Controller.class).asClass());
+        final List<String> controllerClassNames = controllers.stream()
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> services = reflections.get(TypesAnnotated.with(Service.class).asClass());
+        final List<String> servicesClassNames = services.stream()
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
+
+        final Set<Class<?>> repositories = reflections.get(TypesAnnotated.with(Repository.class).asClass());
+        final List<String> repositoryClassNames = repositories.stream()
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
+
+        log.info("Controllers : {}", Arrays.toString(controllerClassNames.toArray()));
+        log.info("Services : {}", Arrays.toString(servicesClassNames.toArray()));
+        log.info("Repositories : {}", Arrays.toString(repositoryClassNames.toArray()));
     }
 }
